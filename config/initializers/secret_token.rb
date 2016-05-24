6b2050dcb93680d4649e89e39a9c27efd5fdfab6
lithosphere <calvin.1337@gmail.com>
2016-05-24 19:47:48 +0000

    first commit with rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RottenMangos::Application.config.secret_key_base = '7b8d884d508be3bfec00a368d89b24e0ecfa882bdd2869848464f4aafcb67e4a541b76be1a0907bd0809db78ee1b64e43fb473ae4663ceb1b0ad18552655ebf5'
