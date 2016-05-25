19247f35d79fd69661248259eb2553ff07bfe368
lithosphere <calvin.1337@gmail.com>
2016-05-25 07:12:26 +0000

    added picture adding feature

--- Gemfile
+++ Gemfile
@@ -31,7 +31,8 @@ group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
 end
-
+gem 'mini_magick'
+gem 'carrierwave'
 # Use ActiveModel has_secure_password
 gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,9 +3,11 @@ class Movie < ActiveRecord::Base
   validates :director, presence: true
   validates :runtime_in_minutes, numericality: { only_integer: true }
   validates :description, presence: true
-  validates :poster_image_url, presence: true
+  validates :release_date, presence: true
   validate :release_date_is_in_the_past
   has_many :reviews
+
+  mount_uploader :image, AvatarUploader
 
   def review_average
    reviews.size != 0 ? reviews.sum(:rating_out_of_ten)/reviews.size : "N/A"
